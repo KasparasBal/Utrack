94e75cfb03f04e2bf947ff03131b162ddbb7b1c1
Kasparas Balys <98013126+KasparasBal@users.noreply.github.com>
2022-11-10 19:48:23 +0200

    commit 2: started creator (activity creation)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 const sideBtnOpen = document.querySelector(".sidebar__btn--open");
 const sideBtnClose = document.querySelector(".sidebar__btn--close");
+const sideBtnNew = document.querySelector(".sidebar__btn--new");
 const sidebar = document.querySelector(".sidebar__content");
 
+//Open/Close Sidebar Menu
 const openSide = function () {
-  sidebar.classList.remove("hidden-sidebar");
+  sidebar.classList.contains("hidden-sidebar")
+    ? sidebar.classList.remove("hidden-sidebar")
+    : sidebar.classList.add("hidden-sidebar");
 };
 const closeSide = function () {
   sidebar.classList.add("hidden-sidebar");
 };
 
+//Open Activity Creator
+const openCreator = function () {};
+
 sideBtnOpen.addEventListener("click", openSide);
 sideBtnClose.addEventListener("click", closeSide);
+
+sideBtnNew.addEventListener("click", openCreator);
