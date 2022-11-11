3ccd72244f22e56e9d45c0a522d0d6ee0c1d9420
Kasparas Balys <98013126+KasparasBal@users.noreply.github.com>
2022-11-11 18:00:27 +0200

    Commit 4: added some functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
+//Selections
+///////////////////////////////////////////////////////////////
+
+//Sidebar
 const sideBtnOpen = document.querySelector(".sidebar__btn--open");
 const sideBtnClose = document.querySelector(".sidebar__btn--close");
+const sidebar = document.querySelector(".sidebar__content");
+//Creator
 const sideBtnNew = document.querySelector(".sidebar__btn--new");
 const createBtnClose = document.querySelector(".creator__btn-close");
-const sidebar = document.querySelector(".sidebar__content");
+const createBtnSubmit = document.querySelector(".creator__btn-submit");
 const creator = document.querySelector(".section__creator");
+const creatorForm = document.querySelector(".creator__form");
+//Activity
+const activitiesTitle = document.querySelector(".activities__title");
+const activityList = document.querySelector(".activities");
+const actTitle = document.getElementById("actTitle");
+const actRepTime = document.getElementById("actRepTime");
+const actBreakTime = document.getElementById("actBreakTime");
+const actSets = document.getElementById("actSets");
+const actReps = document.getElementById("actReps");
+
+//Activities Display
+const currActTitle = document.querySelector(".display__title");
+const currActTimer = document.querySelector(".display__timer");
+const currActReps = document.querySelector(".display__reps");
+const currActSets = document.querySelector(".display__sets");
+const activMain = document.querySelector(".activities__main");
+
+//Sidebar
+///////////////////////////////////////////////////////////////
 
 //Open/Close Sidebar Menu
 const openSide = function () {
@@ -31,3 +56,152 @@ sideBtnNew.addEventListener("click", openCreator);
 sideBtnNew.addEventListener("click", openCreator);
 
 createBtnClose.addEventListener("click", closeCreator);
+
+//Activities
+///////////////////////////////////////////////////////////////
+
+class Activity {
+  constructor(title, repTime, breakTime, sets, reps, id) {
+    this.title = title;
+    this.repTime = repTime;
+    this.breakTime = breakTime;
+    this.sets = sets;
+    this.reps = reps;
+    this.id = id;
+  }
+}
+
+//App
+///////////////////////////////////////////////////////////////
+class App {
+  #activities = [];
+  constructor() {
+    creatorForm.addEventListener("submit", this._createNewAct.bind(this));
+    activityList.addEventListener(
+      "click",
+      this._displayCurrentActivity.bind(this)
+    );
+    if (this.#activities.length === 0) this._displayNoActivities();
+  }
+  //Validate Inputs
+  _validateInputs(...inputs) {
+    //Needs Work
+    const validInputs = inputs.every((inp) => Number.isFinite(inp));
+    const positiveNumbers = inputs.every((inp) => inp > 0);
+
+    if (!validInputs || !positiveNumbers) return false;
+    if (validInputs && positiveNumbers) return true;
+  }
+
+  //Create a new activity
+  _createNewAct(e) {
+    //Create activity
+    e.preventDefault();
+    const title = actTitle.value;
+    const repTime = +actRepTime.value;
+    const breakTime = +actBreakTime.value;
+    const sets = +actSets.value;
+    const reps = +actReps.value;
+    const id = title.slice(-2) + (Date.now() + "").slice(5);
+    console.log(id);
+
+    if (
+      !this._validateInputs(repTime, breakTime, sets, reps) ||
+      title.length === 0
+    ) {
+      actTitle.classList.contains("wrongInput")
+        ? actTitle.classList.remove("wrongInput")
+        : actTitle.classList.add("wrongInput");
+    } else {
+      let activity = new Activity(title, repTime, breakTime, sets, reps, id);
+      this.#activities.push(activity);
+      this._displayActivities(activity);
+      this._clearForm();
+      this._LastActivity(activity);
+    }
+  }
+  _LastActivity(activity) {
+    let html = `
+    <section class="section__display">
+    <div class="display__title">${activity.title}</div>
+    <div class="display__timer">${activity.repTime}</div>
+    <div class="display__buttons">
+      <div class="display__btn display__btn--start">Start</div>
+      <div class="display__btn display__btn--skip">Skip</div>
+      <div class="display__btn display__btn--add">+5 min</div>
+      <div class="display__btn display__btn--end">End</div>
+    </div>
+    <div class="display__stats">
+      <div class="display__reps">Reps: 0/${activity.reps}</div>
+      <p class="seperator">|</p>
+      <div class="display__sets">Sets: 0/${activity.sets}</div>
+    </div>
+  </section>`;
+
+    activMain.insertAdjacentHTML("beforeend", html);
+  }
+
+  //Clear form
+  _clearForm() {
+    actTitle.value =
+      actRepTime.value =
+      actBreakTime.value =
+      actSets.value =
+      actReps.value =
+        "";
+  }
+
+  //Display that no activities are present
+  _displayNoActivities() {
+    let html = "Currently you have no activities created.";
+    activitiesTitle.insertAdjacentHTML("afterend", html);
+  }
+
+  //Display Activities
+  _displayActivities(activity) {
+    let html = `
+      <li class="activity" data-id="${activity.id}">
+      <div class="activity__logo">
+        <img
+          src="/img/barbell.svg"
+          alt="activity logo"
+          class="activity__logo"
+        />
+      </div>
+      <aside class="activity__aside">
+        <div class="activity__title">${activity.title}</div>
+        <div class="activity__reps">Reps:${activity.reps}x</div>
+        <div class="activity__sets">
+          <h4 class="activity__sets-text">${activity.sets}x</h4>
+        </div>
+        <div class="activity__meter"></div>
+      </aside>
+    </li>
+      `;
+
+    activitiesTitle.insertAdjacentHTML("afterend", html);
+  }
+
+  //Display Current Activity
+  _displayCurrentActivity(e) {
+    //find clicked activity in the activities array.
+    const activity = e.target;
+    const currAct = this.#activities.find((act) => {
+      act.id === activity.closest(".activity").dataset.id;
+    });
+    //if not found
+    if (!currAct) return;
+    //if found
+    currActTitle.textContent = currAct.title;
+    currActReps.textContent = currAct.reps;
+    currActSets.textContent = currAct.sets;
+  }
+
+  //Getters
+  //Get Activities
+  get activities() {
+    return this.#activities;
+  }
+}
+
+const app = new App();
--- js/sidebar.js
+++
@@ -1,9 +0,0 @@
-const sideBtnOpen = document.querySelector("sidebar__btn--open");
-const sideBtnClose = document.querySelector("sidebar__btn--close");
-const sidebar = document.querySelector("sidebar__content");
-
-const openSide = function () {
-  sidebar.classList.remove("hidden");
-};
-
-sideBtnOpen.addEventListener("click", openSide);
